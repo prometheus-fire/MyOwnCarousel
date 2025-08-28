bdd04793bc958c18189aa3596e5a123d36b7e831
prometheus-fire <andrea_angelici@outlook.it>
2025-08-28 10:17:40 +0200

    now the project should compile at the first try

--- MyOwnCarousel/src/user_interface.h
+++ MyOwnCarousel/src/user_interface.h
@@ -4,8 +4,8 @@
 #include <string>
 
 #include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include <GLFW/glfw3.h>
 
